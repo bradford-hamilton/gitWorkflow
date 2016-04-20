9c184dbafadc7315fe8f0a6c17aba68cc7513f17
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-04-20 13:02:19 -0600

    jquery change

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("button").click(function() {
+  $("button").on('click', function() {
     $("#watermelon").animate({left: "+=500"}, 2000);
     $("#watermelon").animate({left: "-=300"}, 1000);
   })
